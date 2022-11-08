a8a5619d981819eee41491c69fdb8019f342d5ff
George Dickinson <george.dickinson@gmail.com>
2022-11-07 18:38:12 -0700

    piezo1 analysis added
    Tools for analysing Gabby's piezo1 microscope data. Makes use of code supplied by Vivek for feature analysis and SVM classification. Also Kyle's linking algorithm.

---
+++ piezo1_analysis/workFlow/Step_3-5_exportLagsToOrigin.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  5 17:25:00 2022
+
+@author: george
+"""
+import numpy as np
+import pandas as pd
+
+from tqdm import tqdm
+import os, glob
+
+
+def makeLagDF(exptFolder):
+    lagFiles = glob.glob(exptFolder + '/**/*_lagsHisto.txt', recursive = True)  
+    lagDF = pd.DataFrame()
+    
+    #add lags to df
+    for lagFile in tqdm(lagFiles):
+        tempDF = pd.read_csv(lagFile,names=['lag'])
+        colName = os.path.basename(lagFile).split('_MMStack')[0]
+        lagDF[colName] = tempDF['lag']
+
+
+    #df squared
+    lagSquaredDF = np.square(lagDF)
+    
+    #export lagDF
+    savename = os.path.join(exptFolder, '{}_lags.csv'.format(exptFolder.split('/')[-1]))
+    savename2 = os.path.join(exptFolder, '{}_lagsquared.csv'.format(exptFolder.split('/')[-1]))
+    lagDF.to_csv(savename, float_format='%.5f')
+    print ('{} exported'.format(savename))
+    lagSquaredDF.to_csv(savename2, float_format='%.5f')
+    print ('{} exported'.format(savename2))    
+    
+
+
+if __name__ == '__main__':
+    ##### RUN ANALYSIS        
+    path = '/Users/george/Data/10msExposure10s'
+    
+    #get expt folder list
+    exptList = glob.glob(path + '/*', recursive = True)   
+
+    for exptFolder in tqdm(exptList):
+        makeLagDF(exptFolder)
+
+
+
+
+
+
+
+        
